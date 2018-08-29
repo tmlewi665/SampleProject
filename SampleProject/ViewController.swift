81680525ebcba4a08c19a0a887d7a1b7426bf4bb
Tyler Lewis <tmlewi665@yahoo.com>
2018-08-29 18:57:02 -0500

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Tyler Lewis on 8/29/18.
+//  Copyright © 2018 Tyler Lewis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
